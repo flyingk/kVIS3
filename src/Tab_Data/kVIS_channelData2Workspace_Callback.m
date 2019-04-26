bfa87a8816e335ebec7852730d2a71b509569afc
Kai Lehmkuehler <kai@uav-flightresearch.com>
2019-04-26 15:48:04 +0200

    data export from channel menu prepare

---
+++ src/Tab_Data/kVIS_channelData2Workspace_Callback.m
@@ -0,0 +1,46 @@
+% kVIS3 Data Visualisation
+%
+% Copyright (C) 2012 - present  Kai Lehmkuehler, Matt Anderson and
+% contributors
+%
+% Contact: kvis3@uav-flightresearch.com
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function kVIS_channelData2Workspace_Callback(hObject, ~)
+
+disp('To be implemented...')
+
+% handles = guidata(hObject);
+% 
+% [ fds ] = kVIS_getCurrentFds(hObject);
+% 
+% 
+% groupNo = handles.uiTabData.groupTree.SelectedNodes.Value;
+% 
+% groupName = fds.fdata{fds.fdataRows.groupLabel, groupNo};
+% 
+% 
+% channelNo = handles.uiTabData.channelListbox.Value;
+% 
+% channelName = fds.fdata{fds.fdataRows.varNames, groupNo}{channelNo};
+% 
+% IDstr = [groupName '/' channelName];
+% 
+% clipboard('copy', IDstr)
+% 
+% disp(['Copied ' IDstr ' to clipboard'])
+
+end
+
