bc00c9643633ae3ff3951f8ca05831576c49fd87
Kai Lehmkuehler <kai@uav-flightresearch.com>
2019-05-08 11:16:45 +0200

    updates to line modification/ undo

--- src/Common/kVIS_plotLineContextMenuActions_Callback.m
+++ src/Common/kVIS_plotLineContextMenuActions_Callback.m
@@ -4,28 +4,34 @@
 % contributors
 %
 % Contact: kvis3@uav-flightresearch.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 function kVIS_plotLineContextMenuActions_Callback(source, ~, line)
+%
+% provide functionality to modify a plot line
+%
 
-    if ~isstruct(line.UserData)
-        line.UserData = struct();
-    end
+if ~isstruct(line.UserData)
+    disp('No line data available...')
+    return
+end
 
-    switch source.Label
-        
+switch source.Label
+    %
+    % change line thickness
+    %
     case 'Highlight'
         if isfield(line.UserData, 'HighlightState') && line.UserData.HighlightState
             line.LineWidth = 0.5;
@@ -35,41 +41,76 @@ function kVIS_plotLineContextMenuActions_Callback(source, ~, line)
             line.UserData.HighlightState = true;
         end
         
+    %
+    % apply a math operation to the line data
+    %
     case 'Modify'
         
-        y = line.YData;
+        % get line data as column vector
+        y = line.YData';
         
+        % ask for desired operation
         answ = inputdlg({'Enter Operator (y = line data):','Optional operator channel (group/channel) (z variable):'});
         
         if ~isempty(answ)
             
+            % save state to restore
+            line.UserData.dataBackup = y';
+            line.UserData.nameBackup = line.DisplayName;
+            line.UserData.colorBackup = line.Color;
+            line.UserData.modState = true;
+            
+            % second channel as operator?
             if ~isempty(answ{2})
                 fds = kVIS_getCurrentFds(source);
                 zChanID = strsplit(answ{2}, '/');
-                z = kVIS_fdsGetChannel(fds, zChanID{1}, zChanID{2});
+                z = kVIS_fdsGetChannel(fds, zChanID{1}, strip(zChanID{2}));
                 
                 if z == -1
                     disp('y channel not found... Skipping.')
                     return
                 end
-                
-                z = z';
             end
             
+            %
+            % evaluate requested expression
+            %
             res = eval(answ{1});
             
+            %
+            % update line - change color and name to indicate modifcation
+            %
             line.YData = res;
-            line.LineStyle = '-.';
-            line.LineWidth = 1.5;
+            line.LineStyle = '-';
+            line.Color = 'm';
             
             line.DisplayName = [line.DisplayName '_MOD_(' answ{1} ')'];
-            
+               
+        else
+            % do nothing
+            return
+        end
+    
+    %
+    % undo line math operation
+    %
+    case 'Undo modify'
+        
+        if line.UserData.modState == true
+        
+            line.YData = line.UserData.dataBackup;
+            line.DisplayName = line.UserData.nameBackup;
+            line.Color = line.UserData.colorBackup;
+            line.UserData.modState = false;
         end
         
+    %
+    % delete the line
+    %
     case 'Delete'
         delete(line);
     otherwise
         warning('Unknown action: %s', source.Label);
-    end
+end
 
 end
