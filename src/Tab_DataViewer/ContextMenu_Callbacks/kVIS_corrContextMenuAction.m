6357ea19c9f9bdb9a3d1f0101b1ae7afbe9f1184
Kai Lehmkuehler <kai@uav-flightresearch.com>
2019-07-04 09:32:53 +0200

    catch NaN

--- src/Tab_DataViewer/ContextMenu_Callbacks/kVIS_corrContextMenuAction.m
+++ src/Tab_DataViewer/ContextMenu_Callbacks/kVIS_corrContextMenuAction.m
@@ -4,17 +4,17 @@
 % contributors
 %
 % Contact: kvis3@uav-flightresearch.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,7 +23,7 @@ function kVIS_corrContextMenuAction(hObject, ~, ax)
 switch hObject.Label
     
     case 'Force Update'
-
+        
         kVIS_correlationPlot([], []);
         
     case 'Simple Fitting'
@@ -33,9 +33,17 @@ switch hObject.Label
         X = ax.Children.XData;
         Y = ax.Children.YData;
         
-        pp = polyfit(X,Y,1)
-        
-        plot(ax, X, polyval(pp, X),'k')
+        if ~isnan(X) | ~isnan(Y)
+            
+            pp = polyfit(X,Y,1)
+            
+            plot(ax, X, polyval(pp, X),'k')
+            
+        else
+            
+            errordlg('Data contains NaN. Abort fitting.')
+            
+        end
         
 end
 
