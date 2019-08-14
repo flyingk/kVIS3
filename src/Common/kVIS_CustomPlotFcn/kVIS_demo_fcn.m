a9a7e47ffc719ff50027117e141cc2876e6b3f68
Kai Lehmkuehler <kai@uav-flightresearch.com>
2019-08-14 16:50:33 +0200

    update demo fcn

--- src/Common/kVIS_CustomPlotFcn/kVIS_demo_fcn.m
+++ src/Common/kVIS_CustomPlotFcn/kVIS_demo_fcn.m
@@ -1,37 +1,18 @@
-function y = kVIS_demo_fcn(data, varargin)
+function [y, x] = kVIS_demo_fcn(data, varargin)
+
+x = [];
 
 % first argument is the data structure
 fds  = varargin{1};
 
+% second argument is the data range (if set)
+pts  = varargin{2};
+
 % separate argument string supplied from spreadsheet
-args = strsplit(varargin{2}, ',');
+args = strsplit(varargin{3}, ',');
 
 
 % function specific code
-timeChPath = args{1};
-fc         = str2double(args{2});
-
-ccF = strsplit(timeChPath, '/');
-timeCh = kVIS_fdsGetChannel(fds, ccF{1}, ccF{2});
-
-dt = mean(diff(timeCh));
-
-
-
-y = fdfilt(data, fc, dt);
+y = data;
 
 end
-
-
-%         % check if operator is numeric constant or channel name
-%         if ~isnumeric(plotDef{i, fcnChannel})
-%             
-%             
-%             if fcnData == -1
-%                 disp('Function channel not found... Ignoring.')
-%                 k=k-1;
-%                 continue;
-%             end
-%         else
-%             fcnData = ones(length(yp),1) * plotDef{i, fcnChannel};
-%         end
