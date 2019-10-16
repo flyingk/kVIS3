3548cd37a5b0441d233984919fce0bead8ac0f49
Kai Lehmkuehler <kai@uav-flightresearch.com>
2019-10-16 17:22:44 +0200

    move stuff

--- src/Tab_DataReplay/mex/compile.m
+++
@@ -1,6 +0,0 @@
-clc
-
-% do the mex file
-mex('mavlink_udp_mex.c')
-
-disp('Done.')
--- src/Tab_DataReplay/mex/test.m
+++
@@ -1,7 +0,0 @@
-clc
-
-mavlink_udp_mex('UDP_Init')
-
-mavlink_udp_mex('UDP_Transmit')
-
-mavlink_udp_mex('UDP_Close')
