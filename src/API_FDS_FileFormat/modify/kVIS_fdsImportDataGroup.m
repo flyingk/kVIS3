02cf192c2bdb652ac226eecad0697dcbe3fe58e6
Kai Lehmkuehler <kai@uav-flightresearch.com>
2020-02-12 09:40:02 +0100

    select name for imported data group

--- src/API_FDS_FileFormat/modify/kVIS_fdsImportDataGroup.m
+++ src/API_FDS_FileFormat/modify/kVIS_fdsImportDataGroup.m
@@ -1,3 +1,35 @@
+%
+%> @file kVIS_fdsImportDataGroup.m
+%> @brief Add data from a CSV file to the current data tree
+%
+%
+% kVIS3 Data Visualisation
+%
+% Copyright (C) 2012 - present  Kai Lehmkuehler, Matt Anderson and
+% contributors
+%
+% Contact: kvis3@uav-flightresearch.com
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%
+%> @brief Add data from a CSV file to the current data tree
+%>
+%> @param Application handle structure
+%>
+%> @retval none
+%
 function kVIS_fdsImportDataGroup(hObject, ~)
 %
 % Import data from csv file and add to current fds tree
@@ -51,10 +83,15 @@ varUnits = cellfun(@(x) '', varUnits, 'UniformOutput', false);
 varFrames = cell(size(varUnits));
 varFrames = cellfun(@(x) '', varFrames, 'UniformOutput', false);
 
-fds = kVIS_fdsAddDataGroup(fds, 'Imported', 'AirData', varNames, varUnits, varFrames, fdata);
+% select location in data tree
+answ = inputdlg({'New Tree branch','New Group name'},'Select import names',1,{'Imported','FTI Data'});
 
+if isempty(answ)
+    return
+end
 
+% add data to tree
+fds = kVIS_fdsAddDataGroup(fds, answ{1}, answ{2}, varNames, varUnits, varFrames, fdata);
 
 kVIS_updateDataSet(hObject, fds, name)
-
 end
