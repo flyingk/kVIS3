9e14dbfc275edef3477e51a07cadc86859f37dfe
Kai Lehmkuehler <kai@uav-flightresearch.com>
2020-08-31 14:32:30 +0200

    beta 009

--- src/UI/kVIS_uiSetupFramework.m
+++ src/UI/kVIS_uiSetupFramework.m
@@ -57,7 +57,7 @@ appWindow = figure( ...
 %
 % Assign the a name to appear in the window title.
 %
-set(appWindow,'name',['kVIS3 beta 008 - BSP: ' BSP_Info.Name ' build ' BSP_Info.Version]);
+set(appWindow,'name',['kVIS3 beta 009 - BSP: ' BSP_Info.Name ' build ' BSP_Info.Version]);
 
 %
 % get handle structure
