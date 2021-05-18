c278ebf11a4d35ad737d4a90968b42c7718d8991
Kai Lehmkuehler <kai@uav-flightresearch.com>
2021-05-18 08:56:07 +0200

    bin format option

--- src/Common/DataCursor_Callbacks/kVIS_dataCursor_Callback.m
+++ src/Common/DataCursor_Callbacks/kVIS_dataCursor_Callback.m
@@ -8,7 +8,7 @@ function output_txt = kVIS_dataCursor_Callback(obj,event_obj)
 pos = get(event_obj,'Position');
 
 % format output
-answ = inputdlg({'Digits X', 'Digits Y', 'Number format [dec or hex]'}, 'Data tip format', 1, {'6', '6', 'dec'});
+answ = inputdlg({'Digits X', 'Digits Y', 'Number format [dec, hex or bin]'}, 'Data tip format', 1, {'6', '6', 'dec'});
 
 if strcmp(answ{3}, 'dec')
     
@@ -24,6 +24,16 @@ elseif strcmp(answ{3}, 'hex')
     output_txt = {...
         ['X: ',num2str(pos(1), str2double(answ{1}) )],...
         ['Y: ',['0x' hexstr]]};
+    
+elseif strcmp(answ{3}, 'bin')
+    
+    a = pos(2);
+    binstr = dec2bin(round(a));
+    
+    output_txt = {...
+        ['X: ',num2str(pos(1), str2double(answ{1}) )],...
+        ['Y: ',['0b' binstr]]};
+    
 else
     errordlg('Wrong number format for datatip.')
 end
