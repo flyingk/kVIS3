d5ca923f3751dcb94f0654b456789b0341063b65
Kai Lehmkuehler <kai@uav-flightresearch.com>
2021-05-18 03:35:49 +0200

    variable data tip format

--- src/Common/DataCursor_Callbacks/kVIS_dataCursor_Callback.m
+++ src/Common/DataCursor_Callbacks/kVIS_dataCursor_Callback.m
@@ -4,11 +4,33 @@ function output_txt = kVIS_dataCursor_Callback(obj,event_obj)
 % event_obj    Handle to event object
 % output_txt   Data cursor text string (string or cell array of strings).
 
+% get data
 pos = get(event_obj,'Position');
-output_txt = {['X: ',num2str(pos(1),8)],...
-    ['Y: ',num2str(pos(2),4)]};
+
+% format output
+answ = inputdlg({'Decimals X', 'Decimals Y', 'Number format [dec or hex]'}, 'Data tip format', 1, {'6', '6', 'dec'});
+
+if strcmp(answ{3}, 'dec')
+    
+    output_txt = {...
+        ['X: ',num2str(pos(1), str2double(answ{1}) )],...
+        ['Y: ',num2str(pos(2), str2double(answ{2}) )]};
+    
+elseif strcmp(answ{3}, 'hex')
+    
+    a = pos(2);
+    hexstr = dec2hex(round(a));
+    
+    output_txt = {...
+        ['X: ',num2str(pos(1), str2double(answ{1}) )],...
+        ['Y: ',hexstr]};
+else
+    errordlg('Wrong number format for datatip.')
+end
 
 % If there is a Z-coordinate in the position, display it as well
 if length(pos) > 2
-    output_txt{end+1} = ['Z: ',num2str(pos(3),4)];
+    output_txt{end+1} = ['Z: ',num2str(pos(3),8)];
+end
+
 end
