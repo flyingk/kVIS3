2e087c510a034dffbef509a6706720ed2ba78573
Dr Kai Lehmkuehler <kai.lehmkuehler@lilium.com>
2022-05-05 16:49:17 +0200

    add video player sketch

--- src/Common/VideoDev/kVIS_fdsImportVideo.m
+++ src/Common/VideoDev/kVIS_fdsImportVideo.m
@@ -9,17 +9,17 @@
 % contributors
 %
 % Contact: kvis3@uav-flightresearch.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -30,38 +30,102 @@
 %> @param App events
 %>
 %
+
 function kVIS_fdsImportVideo(hObject, ~)
 
 clc
 
 [fds, name] = kVIS_getCurrentFds(hObject);
 
-[file, path] = uigetfile('*.mp4');
+
+% [file, path] = uigetfile('*.mp4');
 
 vid = VideoReader(fullfile(path, file));
+vid2 = VideoReader(fullfile(path, file2));
 
+duration = vid.Duration
+
+fr = vid.FrameRate
+
+% nf = vid.NumFrames
+% 
+% nf/fr
+
+nf = duration*fr - 1
 
 % video player figure
-fig = figure();
+fig = figure('Position',[305,467,1622,595.34]);
 
-ax = axes(fig);
+vBox = uix.VBox('Parent',fig);
 
-uicontrol(fig, 'Style', 'pushbutton',...
+pltBox = uix.HBox('Parent', vBox);
+axBox = uix.Panel('Parent', pltBox);
+ax2Box = uix.Panel('Parent', pltBox);
+
+sldBox = uix.HButtonBox('Parent', vBox);
+
+sldBox.ButtonSize = [400 40];
+
+btnBox = uix.HButtonBox('Parent', vBox);
+
+btnBox.ButtonSize = [50 50];
+
+vBox.Heights = [-1 50 50];
+
+ax = axes(axBox);
+axis tight
+ax2 = axes(ax2Box);
+axis tight
+
+pauseBtn = uicontrol(btnBox, 'Style', 'pushbutton',...
     'CData', imread('icons8-pause-32.png'),...
-    'Position',[100 100 50 50]);
+    'Callback', @pauseCallback);
+
+playBtn = uicontrol(btnBox, 'Style', 'pushbutton',...
+    'CData', imread('icons8-play-32.png'),...
+    'Callback', @playCallback);
+
+sliderHandle = uicontrol(sldBox, 'Style', 'slider',...
+    'Min', 1, 'Max', nf, 'Value', 1,...
+    'SliderStep', [1/nf,60/nf], 'Tag', 'sliderMov');
+
+sliderHandle.UserData.mov = vid;
+sliderHandle.UserData.mov2 = vid2;
+sliderHandle.UserData.ax  = ax;
+sliderHandle.UserData.ax2  = ax2;
+sliderHandle.UserData.offset = 2.2974364e+04;
+sliderHandle.UserData.offset = 0;
+sliderHandle.UserData.offset2 = 0;
+
+sliderListener = addlistener(sliderHandle, 'ContinuousValueChange', ...
+    @sliderCallback);
+
+tim = timer;
+tim.ExecutionMode = 'fixedRate';
+tim.TasksToExecute = 10;
+tim.BusyMode = 'drop';
+tim.Period = 0.1;%1/fr;
+tim.TimerFcn = @timerCallback;
+
+sliderHandle.UserData.timer = tim;
+playBtn.UserData.timer = tim;
+pauseBtn.UserData.timer = tim;
+
+% while hasFrame(vid)
+
+f = readFrame(vid);
+
+sliderHandle.UserData.img = imshow(f, 'Parent', ax);
+
+sliderHandle.UserData.timeCode = text(ax, 0.45, 0.1, num2str(vid.CurrentTime,7), 'FontSize', 40, 'Color', 'r', 'Units', 'normalized');
 
 
-while hasFrame(vid)
-    
-    f = readFrame(vid);
-    
-    imshow(f, 'Parent', ax, 'Border','tight')
-    
-    timeCode = text(ax, 0.45, 0.1, num2str(vid.CurrentTime), 'FontSize', 40, 'Color', 'r', 'Units', 'normalized');
-    
-    pause(1/vid.FrameRate);
-    
-end
+f2 = readFrame(vid2);
+
+sliderHandle.UserData.img2 = imshow(f2, 'Parent', ax2);
+
+sliderHandle.UserData.timeCode2 = text(ax2, 0.45, 0.1, num2str(vid2.CurrentTime,7), 'FontSize', 40, 'Color', 'r', 'Units', 'normalized');
+
 
 
 % Need to save video info, not object - it saves absolute path to video
@@ -71,8 +135,76 @@ videoObj.filename = []; % file, path relative to fds. Where is fds on disk???
 videoObj.timeOffset= 0;
 
 % fds.linkedVideo = vid;
-% 
+%
 % kVIS_updateDataSet(hObject, fds, name);
 
 end
 
+
+function playCallback(obj,~)
+obj.UserData.timer.Period = 0.017;
+obj.UserData.timer.TasksToExecute = 2000;
+obj.UserData.timer.start;
+end
+
+function pauseCallback(obj,~)
+obj.UserData.timer.stop;
+end
+
+function sliderCallback(obj,~)
+
+obj.UserData;
+obj.Value;
+
+obj.UserData.offset = (25944.04-134.8527);
+obj.UserData.offset2 = (25944.04-128.3362);
+
+f = read(obj.UserData.mov, round(obj.Value));
+
+obj.UserData.img.CData = f;
+
+obj.UserData.timeCode.String = num2str(obj.UserData.offset + obj.UserData.mov.CurrentTime,7);
+
+
+f2 = read(obj.UserData.mov2, round(obj.Value)-6.5*60);
+
+obj.UserData.img2.CData = f2;
+
+obj.UserData.timeCode2.String = num2str(obj.UserData.offset2 + obj.UserData.mov2.CurrentTime,7);
+
+drawnow limitrate
+
+end
+
+function timerCallback(obj,~)
+%tic
+
+oo = findobj(gcf, 'Tag','sliderMov');
+
+if oo.UserData.mov.hasFrame && oo.UserData.mov2.hasFrame
+    
+    oo.Value = oo.Value + 1;
+    
+    
+    f = readFrame(oo.UserData.mov);
+    
+    oo.UserData.img.CData = f;
+    
+    oo.UserData.timeCode.String = num2str(oo.UserData.offset + oo.UserData.mov.CurrentTime,7);
+    
+    
+    f = readFrame(oo.UserData.mov2);
+    
+    oo.UserData.img2.CData = f;
+    
+    oo.UserData.timeCode2.String = num2str(oo.UserData.offset2 + oo.UserData.mov2.CurrentTime,7);
+    
+    
+    drawnow limitrate
+    
+else
+    obj.stop;
+end
+
+%toc
+end
